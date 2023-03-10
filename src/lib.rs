0d5e6f78acef29116cc041c28b430ba9d22b3d59
BKSalman <kfoo2023@gmail.com>
2023-03-11 02:29:55 +0300

    add s3 storage to app state, add env filters to tracing subscriber

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 use axum::extract::FromRef;
 use jwt_simple::prelude::HS256Key;
 use once_cell::sync::Lazy;
+use s3::interface::Storage;
 use sqlx::PgPool;
 
 pub mod middlewares;
 pub mod posts;
+pub mod s3;
 pub mod users;
+pub mod utils;
 
 #[derive(Clone, FromRef)]
 pub struct AppState {
     pub db: PgPool,
+    pub storage: Storage,
 }
 
 pub static JWT_KEY: Lazy<HS256Key> = Lazy::new(|| HS256Key::generate());
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use jwt_simple::prelude::HS256Key;
-use once_cell::sync::Lazy;
 use rmusawarah::{
+    posts::routes::{create_post, get_post, get_posts},
+    s3::helpers::setup_storage,
     users::routes::{create_user, get_user, get_user_posts, login},
     AppState,
 };
 use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{
+    prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter,
+};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -21,17 +28,27 @@ async fn main() {
         .await
         .expect("db connection");
 
-    let app_state = AppState { db: pool };
+    let app_state = AppState {
+        db: pool,
+        storage: setup_storage().expect("storage"),
+    };
 
     let user_router = Router::new()
+        .route("/", get(get_user))
         .route("/", post(create_user))
         .route("/login", post(login))
-        .route("/:username", get(get_user_posts))
-        .route("/", get(get_user));
+        .route("/:username", get(get_user_posts));
+
+    let posts_router = Router::new()
+        .route("/", post(create_post))
+        .route("/:post_id", get(get_post))
+        .route("/cursor/:cursor", get(get_posts));
 
     let app = Router::new()
         .route("/", get(root))
         .nest("/api/users", user_router)
+        .nest("/api/posts", posts_router)
+        .layer(TraceLayer::new_for_http())
         .with_state(app_state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 6060));
