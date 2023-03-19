95a1777924364b88e02df28395c0663d9bb3e17f
BKSalman <kfoo2023@gmail.com>
2023-03-19 19:58:02 +0300

    get post by id and username (UI WIP)

--- client/src/routes/[username]/[post_id]/+page.svelte
+++ client/src/routes/[username]/[post_id]/+page.svelte
@@ -1,10 +1,14 @@
 <script>
+    export let data;
 
+    const { post } = data;
 </script>
 
 
 <div class="post-container">
-
+    <h1>{post.title}</h1>
+    <div>{post.content}</div>
+    <img src={`https://pub-57f47fc2081648efaa3f351802d15c5d.r2.dev/${post.image.path}`} alt="alo"/>
 </div>
 
 <style>
