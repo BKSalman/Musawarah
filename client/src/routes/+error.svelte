1377d09a0ff8b350349866a4df5f479e8f378ffa
BKSalman <kfoo2023@gmail.com>
2023-03-19 19:58:20 +0300

    styling error page

--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
     import { page } from '$app/stores';
 
-    let errors = $page.error?.errors;
+    const errors = $page.error?.errors;
+    const status = $page.status
 </script>
   
-<h1>
+<h1 class="container">
+    <h1>{status}</h1>
     {#each errors || [] as error}
         <div>{error}</div>
     {/each}
 </h1>
+
+<style>
+.container {
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+</style>
