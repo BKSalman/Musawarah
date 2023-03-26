36ba5b73585e9df42bf7b4a08cec519be24fc8fe
BKSalman <kfoo2023@gmail.com>
2023-03-27 01:41:14 +0300

    gallery style changes

--- client/src/lib/components/Gallery.svelte
+++ client/src/lib/components/Gallery.svelte
@@ -1,17 +1,24 @@
-<script>
+<script lang="ts">
+  import type { PostResponse } from "../../../../bindings/PostResponse";
+  import GalleryItem from "./GalleryItem.svelte";
 
+  export let posts: PostResponse[];
 </script>
 
 <div class="gallery">
-    <slot></slot>
+  {#each posts as post}
+    <GalleryItem {post} />
+  {/each}
 </div>
 
 <style>
-.gallery {
-  display: grid;
-  margin: auto;
-  gap: 0.25rem;
-  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
-  grid-auto-rows: 240px;
-}
+  .gallery {
+    display: grid;
+    margin: auto;
+    gap: 0.25rem;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    grid-auto-rows: 240px;
+    height: 100vh;
+    width: 100%;
+  }
 </style>
--- client/src/lib/components/GalleryItem.svelte
+++ client/src/lib/components/GalleryItem.svelte
@@ -1,29 +1,36 @@
-<script>
-export let imageUrl = "";
+<script lang="ts">
+  import type { PostResponse } from "../../../../bindings/PostResponse";
+
+  export let post: PostResponse;
 </script>
 
-<div class="gallery-item">
-    <img src={imageUrl} alt="">
-</div>
+<a class="gallery-item" href={`/${post.user.username}/${post.id}`}>
+  <img
+    src={`https://pub-57f47fc2081648efaa3f351802d15c5d.r2.dev/${post.image.path}`}
+    alt=""
+  />
+</a>
 
 <style>
-.gallery-item {
-  position: relative;
-  display: block;
-  overflow: hidden;
-  margin: 0;
-}
+  .gallery-item {
+    position: relative;
+    display: block;
+    overflow: hidden;
+    margin: 0;
+  }
 
-.gallery-item img {
-  /* display: block;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  min-height: 100%;
-  min-width: 100%;
-  transform: translate(-50%, -50%); */
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
+  .gallery-item img {
+    /*
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      min-height: 100%;
+      min-width: 100%;
+      transform: translate(-50%, -50%);
+    */
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 </style>
