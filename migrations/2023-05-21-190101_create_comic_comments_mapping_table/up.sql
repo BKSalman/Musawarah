49cc4666b8fc076ee592bfad1eb74acd4ad25177
BKSalman <salman.f.abuhaimed@gmail.com>
2023-06-03 01:14:48 +0300

    fix comic_comments table

--- migrations/2023-05-21-190101_create_comic_comments_mapping_table/up.sql
+++ migrations/2023-05-21-190101_create_comic_comments_mapping_table/up.sql
@@ -3,5 +3,15 @@ CREATE TABLE IF NOT EXISTS comic_comments_mapping (
     parent_comment_id UUID NOT NULL,
     child_comment_id UUID NOT NULL,
 
+    FOREIGN KEY(parent_comment_id)
+        REFERENCES comic_comments(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY(child_comment_id)
+        REFERENCES comic_comments(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
     PRIMARY KEY(parent_comment_id, child_comment_id)
 );
