e5e3f8549d1e0ef7bc704a1e51d81f2c433fca9a
BKSalman <salman.f.abuhaimed@gmail.com>
2023-06-03 03:38:15 +0300

    chapters: better error handling

--- src/comics/chapters/mod.rs
+++ src/comics/chapters/mod.rs
@@ -2,7 +2,7 @@ pub mod models;
 pub mod routes;
 mod utils;
 
-use axum::{http::StatusCode, response::IntoResponse, Json};
+use axum::{http::StatusCode, response::IntoResponse};
 use diesel::result::{DatabaseErrorKind, Error::DatabaseError};
 use serde::Deserialize;
 use utoipa::IntoParams;
@@ -53,48 +53,100 @@ impl IntoResponse for ChaptersError {
         tracing::error!("{:#?}", self);
 
         match self {
-            ChaptersError::ChapterNotFound => {
-                (StatusCode::NOT_FOUND, Json(self.to_string())).into_response()
-            }
-            ChaptersError::BadRequest => {
-                (StatusCode::BAD_REQUEST, Json(self.to_string())).into_response()
-            }
-            ChaptersError::ImageTooLarge => {
-                (StatusCode::BAD_REQUEST, Json(self.to_string())).into_response()
-            }
-            ChaptersError::Conflict(_) => {
-                (StatusCode::CONFLICT, Json(self.to_string())).into_response()
-            }
-            ChaptersError::Diesel(diesel_error) => {
-                if let DatabaseError(DatabaseErrorKind::UniqueViolation, message) = diesel_error {
+            ChaptersError::ChapterNotFound => (
+                StatusCode::NOT_FOUND,
+                ErrorResponse {
+                    error: self.to_string(),
+                    ..Default::default()
+                },
+            )
+                .into_response(),
+            ChaptersError::BadRequest => (
+                StatusCode::BAD_REQUEST,
+                ErrorResponse {
+                    error: self.to_string(),
+                    ..Default::default()
+                },
+            )
+                .into_response(),
+            ChaptersError::ImageTooLarge => (
+                StatusCode::BAD_REQUEST,
+                ErrorResponse {
+                    error: self.to_string(),
+                    ..Default::default()
+                },
+            )
+                .into_response(),
+            ChaptersError::Conflict(_) => (
+                StatusCode::CONFLICT,
+                ErrorResponse {
+                    error: self.to_string(),
+                    ..Default::default()
+                },
+            )
+                .into_response(),
+            ChaptersError::Diesel(diesel_error) => match diesel_error {
+                DatabaseError(DatabaseErrorKind::UniqueViolation, message) => {
                     let constraint_name = message.constraint_name();
                     match constraint_name {
                         Some("comic_chapters_comic_id_number_key") => {
                             return (
                                 StatusCode::CONFLICT,
-                                Json("chapter with same number already exists"),
+                                ErrorResponse {
+                                    error: String::from("chapter with same number already exists"),
+                                    ..Default::default()
+                                },
                             )
                                 .into_response();
                         }
                         Some("chapter_pages_chapter_id_number_key") => {
                             return (
                                 StatusCode::CONFLICT,
-                                Json("chapter page with same number already exists"),
+                                ErrorResponse {
+                                    error: String::from(
+                                        "chapter page with same number already exists",
+                                    ),
+                                    ..Default::default()
+                                },
                             )
                                 .into_response();
                         }
-                        _ => {}
-                    }
-                } else if let DatabaseError(DatabaseErrorKind::ForeignKeyViolation, message) =
-                    diesel_error
-                {
-                    let constraint_name = message.constraint_name();
-                    if let Some("comic_chapters_comic_id_fkey") = constraint_name {
-                        return (StatusCode::NOT_FOUND, Json("comic not found")).into_response();
+                        _ => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
                     }
                 }
-                StatusCode::INTERNAL_SERVER_ERROR.into_response()
-            }
+                DatabaseError(DatabaseErrorKind::ForeignKeyViolation, message) => {
+                    let constraint_name = message.constraint_name();
+                    if let Some("comic_chapters_comic_id_fkey") = constraint_name {
+                        return (
+                            StatusCode::NOT_FOUND,
+                            ErrorResponse {
+                                error: String::from("comic not found"),
+                                ..Default::default()
+                            },
+                        )
+                            .into_response();
+                    }
+                    StatusCode::INTERNAL_SERVER_ERROR.into_response()
+                }
+                diesel::result::Error::QueryBuilderError(message) => {
+                    let message = message.to_string();
+
+                    if message.contains("no changes")
+                    // "There are no changes to save. This query cannot be built"
+                    {
+                        return (
+                            StatusCode::BAD_REQUEST,
+                            ErrorResponse {
+                                error: String::from("no changes"),
+                                ..Default::default()
+                            },
+                        )
+                            .into_response();
+                    }
+                    StatusCode::INTERNAL_SERVER_ERROR.into_response()
+                }
+                _ => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            },
             ChaptersError::InternalServerError => {
                 (StatusCode::INTERNAL_SERVER_ERROR).into_response()
             }
