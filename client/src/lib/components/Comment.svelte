4e01d9fc3611bb43c64b261b70355543561bc9a7
Salman <salman.f.abuhaimed@gmail.com>
2023-07-14 23:35:46 +0300

    add comment input boxes to comic page

--- client/src/lib/components/Comment.svelte
+++ client/src/lib/components/Comment.svelte
@@ -1,13 +1,49 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import type { ComicCommentResponse } from "bindings/ComicCommentResponse";
 
     export let comment: ComicCommentResponse;
     export let indent = 0;
+
+    async function sendComment(e: SubmitEvent, parent_comment_id: string | null) {
+        const form = new FormData(e.target as HTMLFormElement);
+
+        const formComment = form.get("comment");
+
+        if (formComment?.toString() == null || formComment?.toString().length < 1) {
+            return;
+        }
+
+        const res = await fetch(`http://localhost:6060/api/v1/comics/${comment.comic_id}/comments`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                content: formComment,
+                parent_comment_id: parent_comment_id
+            }),
+        });
+
+        if (res.status >= 400) {
+            // TODO: add message to the user that they need to log in
+            await goto("/login");
+        } else {
+            comment.child_comments.push(await res.json() as ComicCommentResponse)
+            // just to force it to refresh
+            comment = comment;
+        }
+    }
 </script>
 
 <div style="padding-left: {indent}rem" class="comment">
     <div class="">{comment.user.username}</div>
     <div class="">{comment.content}</div>
+    <form class="new-reply" on:submit|preventDefault={(e) => sendComment(e, comment.id)}>
+        <input type="text" name="comment" placeholder="Add a reply">
+        <button type="submit">send</button>
+    </form>
     <div class="children">
         {#each comment.child_comments || [] as child}
             <svelte:self comment={child} indent={indent + 0.1}/>
@@ -21,7 +57,7 @@
     margin-left: 15px;
 }
 .comment {
-    background: black;
+    background: #AAAAAA;
     margin-bottom: 10px;
     border-radius: 5px;
     width: 90%;
--- client/src/routes/[username]/[comic_id]/+page.svelte
+++ client/src/routes/[username]/[comic_id]/+page.svelte
@@ -1,12 +1,44 @@
 <script lang="ts">
-    import { page } from '$app/stores';
     import type { PageServerData } from './$types';
     import ChapterThumbnail from '$lib/components/ChapterThumbnail.svelte';
     import Comment from '$lib/components/Comment.svelte';
+    import { currentUser } from '../../stores';
+    import { goto } from '$app/navigation';
+    import type { ComicCommentResponse } from 'bindings/ComicCommentResponse';
 
     export let data: PageServerData;
 
-    const { comic, comments } = data;
+    let { comic, comments } = data;
+    async function sendComment(e: SubmitEvent, parent_comment_id: string | null, comic_id: string) {
+        const form = new FormData(e.target as HTMLFormElement);
+
+        const comment = form.get("comment");
+
+        if (comment?.toString() == null || comment?.toString().length < 1) {
+            return;
+        }
+
+        const res = await fetch(`http://localhost:6060/api/v1/comics/${comic_id}/comments`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                content: comment,
+                parent_comment_id: parent_comment_id
+            }),
+        });
+
+        if (res.status >= 400) {
+            // TODO: add message to the user that they need to log in
+            await goto("/login");
+        } else {
+            comments.unshift(await res.json() as ComicCommentResponse)
+            // just to force it to refresh
+            comments = comments;
+        }
+    }
 </script>
 
 
@@ -25,6 +57,12 @@
         </div>
     <div class="comments">
         <span>comments:</span>
+        {#if $currentUser}
+            <form class="new-comment" on:submit|preventDefault={(e) => sendComment(e, null, comic.id)}>
+                <input type="text" name="comment" placeholder="Add a comment">
+                <button type="submit">send</button>
+            </form>
+        {/if}
         {#each comments as comment}
             <Comment {comment}/>
         {/each}
@@ -33,7 +71,7 @@
 
 <style>
 .comic {
-    background: black;
+    background: #AAAAAA;
     width: 90%;
     margin-bottom: 10px;
 }
