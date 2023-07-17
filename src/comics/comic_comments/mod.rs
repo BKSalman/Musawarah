69c5b3a1055bd6a54f484402289f5917196a8021
mohad12211 <mohammedkaabi64@gmail.com>
2023-07-17 15:24:16 +0400

    add error handling if the comment is not found

--- src/comics/comic_comments/mod.rs
+++ src/comics/comic_comments/mod.rs
@@ -40,6 +40,16 @@ impl IntoResponse for ComicCommentsError {
                         )
                             .into_response();
                     }
+                } else if diesel_err == diesel::result::Error::NotFound {
+                    // TODO: should this be 404 not found?
+                    return (
+                        StatusCode::BAD_REQUEST,
+                        ErrorResponse {
+                            error: String::from("comment id not found"),
+                            ..Default::default()
+                        },
+                    )
+                        .into_response();
                 }
                 StatusCode::INTERNAL_SERVER_ERROR.into_response()
             }
